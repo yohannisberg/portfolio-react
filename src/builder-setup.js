3c674f4027f75eeddbf3fcb4023b8e08fb0977c3
yohannisberg <jdagos3@gmail.com>
2025-10-17 15:51:28 -0500

    edited the portfolio

--- src/builder-setup.js
+++ src/builder-setup.js
@@ -1,17 +1,21 @@
-// IMPORTANT - 
+// IMPORTANT -
 // add "dev": "concurrently \"vite\" \"builder-dev-tools\"" to package.json
 
-import { Builder, builder } from '@builder.io/react';
-import { FancyCallout } from './components/FancyCallout';
+import { Builder, builder } from "@builder.io/react";
+import { FancyCallout } from "./components/FancyCallout";
 
-builder.init('59e38532d569405f84fa31a7843a0d36');
-
-console.log('hi');
+builder.init("59e38532d569405f84fa31a7843a0d36");
+c;
 
 Builder.registerComponent(FancyCallout, {
-  name: 'FancyCallout',
+  name: "FancyCallout",
   inputs: [
-    { name: 'title', type: 'string', defaultValue: 'Hello from React' },
-    { name: 'body', type: 'longText', defaultValue: 'This component was imported into Builder.io' },
+    { name: "title", type: "string", defaultValue: "Hello from React" },
+    {
+      name: "body",
+      type: "longText",
+      defaultValue:
+        "This component was imported into Builder.io, and right now I'm in the feature",
+    },
   ],
 });
